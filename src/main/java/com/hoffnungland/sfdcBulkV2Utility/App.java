084cf02dcf2a073c04ba7686cb56baa92591ac45
manuelmsperanza <manu.speranz@gmail.com>
2023-07-10 20:55:19 +0200

    first working version

--- src/main/java/com/hoffnungland/sfdcBulkV2Utility/App.java
+++ src/main/java/com/hoffnungland/sfdcBulkV2Utility/App.java
@@ -1,13 +1,33 @@
 package com.hoffnungland.sfdcBulkV2Utility;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+		public static void main(String[] args) {
+		
+		String sessionId = "";
+		String baseUrl = "";
+		String jobId = "";
+		String apiVersion = "v58.0";
+		String csvContent = "";
+		try{
+			
+			String response = V2Ingest.checkLimits(sessionId, baseUrl, apiVersion);
+			//String response = V2Ingest.createJob(sessionId, baseUrl, apiVersion, "Offerte_Promozioni__c", "CSV", "update", "SEMICOLON");
+			//String response = V2Ingest.uploadCsvContent(sessionId, baseUrl, apiVersion, jobId, csvContent);
+			//String response = V2Ingest.changeJobStatus(sessionId, baseUrl, apiVersion, jobId, "UploadComplete");
+			
+			System.out.println(response);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
---
+++ src/main/java/com/hoffnungland/sfdcBulkV2Utility/V2Ingest.java
@@ -0,0 +1,125 @@
+package com.hoffnungland.sfdcBulkV2Utility;
+
+import java.io.IOException;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPatch;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.classic.methods.HttpPut;
+import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+
+public class V2Ingest {
+	
+	public static String checkLimits(String sessionId, String baseUrl, String apiVersion) throws IOException {
+		String response = null;
+		
+		try(CloseableHttpClient httpclient = HttpClients.createDefault()){
+			
+			HttpGet getRequest = new HttpGet(baseUrl + "/services/data/" + apiVersion + "/limits");
+			getRequest.addHeader("Authorization", "Bearer " + sessionId);
+			
+			BasicHttpClientResponseHandler responseClientHandler = new BasicHttpClientResponseHandler();
+			response = httpclient.execute(getRequest, responseClientHandler);
+		}
+		
+		return response;
+	}
+	
+	public static String createJob(String sessionId, String baseUrl, String apiVersion, String objectName, String contentType, String operation, String columnDelimiter) throws IOException {
+		String response = null;
+		
+		try(CloseableHttpClient httpclient = HttpClients.createDefault()){
+			
+			HttpPost postRequest = new HttpPost(baseUrl + "/services/data/" + apiVersion + "/jobs/ingest");
+			postRequest.addHeader("Authorization", "Bearer " + sessionId);
+			StringEntity myEntity = new StringEntity("{\"object\" : \"" + objectName + "\", \"contentType\" : \"" + contentType + "\", \"operation\" : \"" + operation + "\", \"columnDelimiter\": \"" + columnDelimiter + "\"}",ContentType.create("application/json", "UTF-8"));
+			postRequest.setEntity(myEntity);
+			BasicHttpClientResponseHandler responseClientHandler = new BasicHttpClientResponseHandler();
+			response = httpclient.execute(postRequest, responseClientHandler);
+		}
+		
+		return response;
+		
+	}
+	
+	public static String uploadCsvContent(String sessionId, String baseUrl, String apiVersion, String jodId, String csvContent) throws IOException {
+		
+		String response = null;
+		
+		try(CloseableHttpClient httpclient = HttpClients.createDefault()){
+			HttpPut putRequest = new HttpPut(baseUrl + "/services/data/" + apiVersion + "/jobs/ingest/" + jodId + "/batches");
+			putRequest.addHeader("Authorization", "Bearer " + sessionId);
+			StringEntity myEntity = new StringEntity(csvContent,ContentType.create("text/csv", "UTF-8"));
+			putRequest.setEntity(myEntity);
+			BasicHttpClientResponseHandler responseClientHandler = new BasicHttpClientResponseHandler();
+			response = httpclient.execute(putRequest, responseClientHandler);
+		}
+		
+		return response;
+	}
+	
+	public static String changeJobStatus(String sessionId, String baseUrl, String apiVersion, String jodId, String status) throws IOException {
+		String response = null;
+		
+		try(CloseableHttpClient httpclient = HttpClients.createDefault()){
+			HttpPatch patchRequest = new HttpPatch(baseUrl + "/services/data/" + apiVersion + "/jobs/ingest/" + jodId);
+			patchRequest.addHeader("Authorization", "Bearer " + sessionId);
+			StringEntity myEntity = new StringEntity("{\"state\" : \"" + status + "\"}",ContentType.create("application/json", "UTF-8"));
+			patchRequest.setEntity(myEntity);
+			BasicHttpClientResponseHandler responseClientHandler = new BasicHttpClientResponseHandler();
+			response = httpclient.execute(patchRequest, responseClientHandler);
+		}
+		
+		return response;
+	}
+	
+	public static String successfulResults(String sessionId, String baseUrl, String apiVersion, String jodId) throws IOException {
+		String response = null;
+		
+		try(CloseableHttpClient httpclient = HttpClients.createDefault()){
+			
+			HttpGet getRequest = new HttpGet(baseUrl + "/services/data/" + apiVersion + "/jobs/ingest/" + jodId + "/successfulResults");
+			getRequest.addHeader("Authorization", "Bearer " + sessionId);
+			
+			BasicHttpClientResponseHandler responseClientHandler = new BasicHttpClientResponseHandler();
+			response = httpclient.execute(getRequest, responseClientHandler);
+		}
+		
+		return response;
+	}
+	
+	public static String failedResults(String sessionId, String baseUrl, String apiVersion, String jodId) throws IOException {
+		String response = null;
+		
+		try(CloseableHttpClient httpclient = HttpClients.createDefault()){
+			
+			HttpGet getRequest = new HttpGet(baseUrl + "/services/data/" + apiVersion + "/jobs/ingest/" + jodId + "/failedResults");
+			getRequest.addHeader("Authorization", "Bearer " + sessionId);
+			
+			BasicHttpClientResponseHandler responseClientHandler = new BasicHttpClientResponseHandler();
+			response = httpclient.execute(getRequest, responseClientHandler);
+		}
+		
+		return response;
+	}
+	
+	public static String unprocessedrecords(String sessionId, String baseUrl, String apiVersion, String jodId) throws IOException {
+		String response = null;
+		
+		try(CloseableHttpClient httpclient = HttpClients.createDefault()){
+			
+			HttpGet getRequest = new HttpGet(baseUrl + "/services/data/" + apiVersion + "/jobs/ingest/" + jodId + "/unprocessedrecords");
+			getRequest.addHeader("Authorization", "Bearer " + sessionId);
+			
+			BasicHttpClientResponseHandler responseClientHandler = new BasicHttpClientResponseHandler();
+			response = httpclient.execute(getRequest, responseClientHandler);
+		}
+		
+		return response;
+	}
+	
+}
